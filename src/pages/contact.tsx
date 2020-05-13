a4aa0477b1f3edd34c14cb03d31953dbad93e7cb
trebeljahr <25333032+trebeljahr@users.noreply.github.com>
2020-05-13 12:45:31 +0200

    Clean up more of the older project structure

---
+++ src/pages/contact.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react"
+import Layout from "../components/layout"
+import { Loader } from "../components/Loader"
+
+const About = () => {
+  return (
+    <Layout color="#4f2223">
+      <div className="container about-container">
+        Hi there this is the contact page!
+      </div>
+    </Layout>
+  )
+}
+
+export default About
